5ce261c3e7f2313ec4f71b3527e38d99ee265e4c
Steven Jeanneret <steven.jeanneret@he-arc.ch>
2019-04-03 18:16:51 +0200

    change url params

--- src/main/java/ch/hearc/sandbox/controller/BoardController.java
+++ src/main/java/ch/hearc/sandbox/controller/BoardController.java
@@ -5,6 +5,7 @@ import ch.hearc.sandbox.model.Board;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Map;
 
@@ -13,8 +14,8 @@ public class BoardController {
     @Autowired
     BoardImpl boardIml;
 
-    @GetMapping("/produits")
-    public String accueil(Map<String, Object> model, Long id) {
+    @GetMapping("/boards/{id}")
+    public String accueil(Map<String, Object> model, @PathVariable Long id) {
         Board board = boardIml.find(id);
         model.put("board", board);
         model.put("posts", boardIml.getPosts(board));
