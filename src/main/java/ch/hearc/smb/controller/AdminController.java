7d51d93148e73f17b31a4a568a79e7d80adf0097
Charles-Lewis jaggi <charles-lewis.jaggi@he-arc.ch>
2019-04-08 22:43:19 +0200

    change role

--- src/main/java/ch/hearc/smb/controller/AdminController.java
+++ src/main/java/ch/hearc/smb/controller/AdminController.java
@@ -3,17 +3,22 @@ package ch.hearc.smb.controller;
 
 import ch.hearc.smb.model.CustomUser;
 import ch.hearc.smb.model.Role;
+import ch.hearc.smb.repository.CustomUserRepository;
 import ch.hearc.smb.repository.RoleRepository;
 import ch.hearc.smb.service.CustomUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 @Controller
@@ -21,6 +26,9 @@ public class AdminController {
 
     @Autowired
     CustomUserService customUserService;
+
+    @Autowired
+    private CustomUserRepository customUserRepository;
 
     @Autowired
     RoleRepository roleRepository;
@@ -46,5 +54,29 @@ public class AdminController {
         return "admin";
     }
 
+    @PutMapping("admin/changerole/{id}")
+    public String changeRole(HttpServletRequest request, @PathVariable Long id) {
+
+        String admin = request.getParameter("admin");
+        String modo = request.getParameter("modo");
+
+        Set<Role> roles = new HashSet<>();
+
+        roles.add(roleRepository.findByName("ROLE_USER"));
+
+        if (admin != null) {
+            roles.add(roleRepository.findByName("ROLE_ADMIN"));
+        }
+
+        if (modo != null) {
+            roles.add(roleRepository.findByName("ROLE_MODO"));
+        }
+
+
+        CustomUser user = customUserService.findByCustomId(id);
+        user.setRoles(roles);
+        customUserRepository.save(user);
+        return "redirect:/admin";
+    }
 }
 
