80af02fee429ca10bd245e79a975a14b3e442792
Charles-Lewis jaggi <charles-lewis.jaggi@he-arc.ch>
2019-04-08 23:05:52 +0200

    update message

--- src/main/java/ch/hearc/smb/controller/AdminController.java
+++ src/main/java/ch/hearc/smb/controller/AdminController.java
@@ -9,15 +9,11 @@ import ch.hearc.smb.service.CustomUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 
@@ -34,7 +30,13 @@ public class AdminController {
     RoleRepository roleRepository;
 
     @GetMapping("/admin")
-    public String admin(Map<String, Object> model) {
+    public String admin(Model model, HttpServletRequest request) {
+
+        String user = request.getParameter("user");
+        if (user != null) {
+            model.addAttribute("roleUpdate", "User " + user + " has been updated");
+        }
+
         return "admin";
     }
 
@@ -76,7 +78,7 @@ public class AdminController {
         CustomUser user = customUserService.findByCustomId(id);
         user.setRoles(roles);
         customUserRepository.save(user);
-        return "redirect:/admin";
+        return "redirect:/admin?user=" + user.getUsername();
     }
 }
 
